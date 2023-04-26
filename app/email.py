02d64293304b8dfd5b952aea785a6bccaaef5ff8
Harshith Devadass <106744429+harshithdeva@users.noreply.github.com>
2023-04-26 07:56:09 +0530

    Update email.py

--- app/email.py
+++ app/email.py
@@ -4,19 +4,19 @@ from flask_mail import Message
 from flask_babel import _
 from app import app, mail
 
-
+# Asynchornous option enabled
 def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
-
+# Defining the email Structure
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
     Thread(target=send_async_email, args=(app, msg)).start()
 
-
+# Enables email support for the application
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
     send_email(_('[Microblog] Reset Your Password'),
